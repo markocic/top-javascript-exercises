18c96976ac0179e4198203d739c5ec42eda3c094
markocic <markokocic2000@gmail.com>
2023-07-31 11:29:40 +0200

    Finished the rest of the assigned exercises.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...params) {
+    let result = array.filter(function (letter) {
+        return !params.includes(letter);
+    });
 
+    return result;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,13 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (typeof x != 'number' || typeof y != 'number') return 'ERROR';
+    if (x < 0 || y < 0) return 'ERROR';
+
+    let sum = 0;
+    for (i = Math.min(x, y); i <= Math.max(x, y); i++) {
+        sum += i;
+    }
+
+    return sum;
 
 };
 
